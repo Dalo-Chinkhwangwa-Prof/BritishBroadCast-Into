824a6e90f11eca283cf79bc813e07248b16c0f80
Dalo-Chinkhwangwa-Prof <Dalo.chinkhwangwa@outlook.com>
2021-02-11 11:51:11 -0500

    Introduction to ConstraintLayout + LinearLayout

--- app/src/main/java/com/britishbroadcast/intro/MainActivity.java
+++ app/src/main/java/com/britishbroadcast/intro/MainActivity.java
@@ -3,20 +3,39 @@ package com.britishbroadcast.intro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mainTextView;
 
+    private int number = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainTextView = findViewById(R.id.main_textview);
+        mainTextView = findViewById(R.id.solution_textview);
+        mainTextView.setText(number+"");
 
-        mainTextView.setText("Hello Dalo");
+    }
+
+    public void onClick(View view){
+
+        switch (view.getId()){
+            case R.id.one_button:
+                if(number > 0)
+                    number = Integer.parseInt(mainTextView.getText().toString() + 1);
+                else
+                    number = 1;
+                mainTextView.setText(number+"");
+                break;
+
+
+        }
+
 
     }
 }
